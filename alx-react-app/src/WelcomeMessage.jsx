c42417cd0acbeb8c5004f5c845a252558c4a05c7
Thabiso Ncube <iamthabisoon@gmail.com>
2024-11-08 11:27:49 +0200

    updated

---
+++ alx-react-app/src/WelcomeMessage.jsx
@@ -0,0 +1,13 @@
+function WelcomeMessage() {
+    return (
+        <WelcomeMessage>
+        <div>
+            <h1>“Hello everyone, I am learning React at ALX!”</h1>
+            <p>"I am learning about JSX!"</p>
+        </div>
+        </WelcomeMessage>
+       
+    );
+}
+
+export default WelcomeMessage;
