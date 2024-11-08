680233a4c74f28975a4663c257b9bf793bb10ef1
Thabiso Ncube <iamthabisoon@gmail.com>
2024-11-08 16:41:53 +0200

    up date

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
-import WelcomeMessage from './WelcomeMessage'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import WelcomeMessage from './WelcomeMessage';
 
+function App() {
+    return <div><WelcomeMessage /></div>
+}
+
+export default App;
