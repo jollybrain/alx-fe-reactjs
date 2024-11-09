3efc9b8957ff8900c5cfb3b3be175640d0af76aa
Thabiso Ncube <iamthabisoon@gmail.com>
2024-11-10 01:49:25 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,8 @@
 import { Component } from 'react';
-import WelcomeMessage from './WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 class App extends Component {
     render() {
@@ -10,6 +13,43 @@ class App extends Component {
         );
     }
     
+}
+
+
+
+// Header.jsx
+import React from'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Welcome to My Website</h1>
+        </header>
+    );
+}
+
+
+// MainContent.jsx
+import React from'react';
+
+const MainContent = () => {
+    return (
+        <main>
+            <p>This is the main content of the website.</p>
+        </main>
+    );
+}
+
+
+// Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>�� 2023 My Website. All rights reserved.</p>
+        </footer>
+    );
 }
 
 export default App;
