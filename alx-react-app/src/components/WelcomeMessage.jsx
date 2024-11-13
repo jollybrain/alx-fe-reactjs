53d73e0fd054c410263611680420676cf03b2f80
Thabiso Ncube <iamthabisoon@gmail.com>
2024-11-13 12:21:25 +0200

    Update WelcomeMessage.jsx

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const WelcomeMessage = () => {
-    //return (
-      //  <div>
-       //     <h1>“Hello everyone, I am learning React at ALX!”</h1>
-       //     <p>"I am learning about JSX!"</p>
-        //</div>
+    return (
+        <div>
+            <h1>“Hello everyone, I am learning React at ALX!”</h1>
+            <p>"I am learning about JSX!"</p>
+        </div>
         
        
-    //)
-    return React.createElement('div', null, '<h1>“Hello everyone, I am learning React at ALX!”</h1>')
+    )
+
 }
 
 export default WelcomeMessage
