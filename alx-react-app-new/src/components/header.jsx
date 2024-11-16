8a10a0a12401044cc8d32dde3dfe6535658504fc
Thabiso Ncube <iamthabisoon@gmail.com>
2024-11-17 00:14:45 +0200

    duclicated

---
+++ alx-react-app-new/src/components/header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Header = () => {
+    return (
+        <Header>
+            <h1>My Favorite Cities</h1>
+        </Header>       
+        
+       
+    )
+
+}
+
+export default Header;
---
+++ alx-react-app-new/src/components/maincontent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const MainContent = () => {
+    return (
+        <MainContent>
+             <p>I love to visit New York, Paris, and Tokyo.</p>
+        </MainContent>
+        
+        
+       
+    )
+
+}
+
+
+export default MainContent
