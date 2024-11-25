fe0fa515f62ee55ac0010dbdf350b68b5f4abd97
Thabiso Ncube <iamthabisoon@gmail.com>
2024-11-25 12:41:27 +0200

    reco and fv

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
 const App = () => {
   return (
@@ -10,13 +12,12 @@ const App = () => {
       <div style={{ padding: '20px' }}>
         <h1>Recipe Sharing App</h1>
         <Routes>
-          {/* Route to the main Recipe List */}
           <Route path="/" element={<RecipeList />} />
-          {/* Route to add a new recipe */}
           <Route path="/add-recipe" element={<AddRecipeForm />} />
-          {/* Route to view details of a specific recipe */}
           <Route path="/recipe/:id" element={<RecipeDetails />} />
         </Routes>
+        <FavoritesList />
+        <RecommendationsList />
       </div>
     </Router>
   );
