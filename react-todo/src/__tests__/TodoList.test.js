75e1dae81f4e269dae405ca5df978fa0f0211bb6
Thabiso Ncube <iamthabisoon@gmail.com>
2024-12-07 16:15:53 +0200

    Update TodoList.test.js

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -13,11 +13,13 @@ describe("TodoList Component", () => {
   test("adds a new todo", () => {
     render(<TodoList />);
     const input = screen.getByPlaceholderText("Add a new todo");
-    const button = screen.getByText("Add");
+    const button = screen.getByRole("button", { name: /add/i });
 
+    // Simulate adding a new todo
     fireEvent.change(input, { target: { value: "New Todo" } });
     fireEvent.click(button);
 
+    // Check if the new todo is added to the list
     expect(screen.getByText("New Todo")).toBeInTheDocument();
   });
 
@@ -25,22 +27,29 @@ describe("TodoList Component", () => {
     render(<TodoList />);
     const todo = screen.getByText("Learn React");
 
-    // Initially not completed
+    // Initially, it should not have a line-through style
     expect(todo).not.toHaveStyle("text-decoration: line-through");
 
+    // Simulate toggle by clicking on the todo
     fireEvent.click(todo);
 
-    // After toggle, it should be completed
+    // After toggling, it should have a line-through style
     expect(todo).toHaveStyle("text-decoration: line-through");
+
+    // Toggle back to not completed
+    fireEvent.click(todo);
+    expect(todo).not.toHaveStyle("text-decoration: line-through");
   });
 
   test("deletes a todo", () => {
     render(<TodoList />);
     const todo = screen.getByText("Learn React");
-    const deleteButton = todo.nextSibling; // The delete button for the first todo
+    const deleteButton = screen.getByRole("button", { name: /delete/i });
 
+    // Simulate deleting the todo
     fireEvent.click(deleteButton);
 
+    // Check if the todo is removed
     expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
   });
 });
