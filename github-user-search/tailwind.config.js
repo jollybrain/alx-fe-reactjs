128da0cc33bff6a8d9a6c37a1e320c1253dd6d9c
Thabiso Ncube <iamthabisoon@gmail.com>
2024-12-25 14:03:35 +0200

    new

---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,11 @@
+
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx}',  // Make sure Tailwind CSS scans your JSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
