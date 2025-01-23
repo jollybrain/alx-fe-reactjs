cf387ba605f831b56c2f4689fe034a73a5a23250
Thabiso Ncube <iamthabisoon@gmail.com>
2025-01-24 00:30:00 +0200

    NEW

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 import Counter from './components/Counter';
 import index from './index';
