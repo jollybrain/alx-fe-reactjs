9cad61860f00d2d31f8081df97d8e83737e35808
Thabiso Ncube <iamthabisoon@gmail.com>
2025-01-24 01:05:40 +0200

    new

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ background: '#333', color: '#fff', padding: '10px', textAlign: 'center' }}>
+        &copy; 2025 Our Company. All Rights Reserved.
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
